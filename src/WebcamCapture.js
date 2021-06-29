085919a7fb1377ad415adee508097882cdb10199
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-28 21:26:14 -0500

    allowed for photo capture

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 const videoConstraints = {
     width: 250,
@@ -10,6 +11,13 @@ const videoConstraints = {
 
 function WebcamCapture() {
     const webcamRef = useRef(null);
+    const [image, setImage] = useState(null);
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        console.log((imageSrc));
+        setImage(imageSrc)
+    }, [webcamRef])
     return (
         <div className='webcamCapture'>
             <Webcam 
@@ -20,6 +28,12 @@ function WebcamCapture() {
                 width={videoConstraints.width}
                 videoConstraints={videoConstraints}
             />
+            <RadioButtonUncheckedIcon 
+                className='webcamCapture__button'
+                onClick={capture}
+                fontsize="Large"
+            />
+            <img src={image} alt=""/>
         </div>
     )
 }
