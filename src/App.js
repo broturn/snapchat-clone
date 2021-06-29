225b3c681874ba6c41d9899e1c50b25d13be7cb3
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-28 22:31:55 -0500

    styling the app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
+import Preview from "./Preview";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Router>
         <div className="app__body">
           <Switch>
+          <Route exact path="/preview">
+              <Preview />
+            </Route>
             <Route exact path="/">
               <WebcamCapture />
             </Route>
