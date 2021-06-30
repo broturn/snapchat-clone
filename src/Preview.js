473a44ba15e6b1776410a74ba856cf1b5e97612b
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-29 22:33:10 -0500

    started wiring up preview fully and installed uuid

--- src/Preview.js
+++ src/Preview.js
@@ -4,7 +4,18 @@ import { useHistory } from "react-router-dom";
 import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
 import "./Preview.css";
 import CloseIcon from "@material-ui/icons/Close";
-import { AttachFile, Create, Crop, MusicNote, Note, Send, TextFields, Timer } from "@material-ui/icons";
+import {
+  AttachFile,
+  Create,
+  Crop,
+  MusicNote,
+  Note,
+  Send,
+  TextFields,
+  Timer,
+} from "@material-ui/icons";
+import { v4 as uuid } from "uuid";
+import { storage } from "./firebase";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -21,6 +32,15 @@ function Preview() {
     dispatch(resetCameraImage());
   };
 
+  const sendPost = () => {
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, "data_url");
+
+      uploadTask.on('state_changed')
+  };
+
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
@@ -34,9 +54,9 @@ function Preview() {
         <Timer />
       </div>
       <img src={cameraImage} alt="" />
-      <div className="preview__footer">
+      <div onClick={sendPost} className="preview__footer">
         <h2>Send Now</h2>
-        <Send fontSize="small" className='preview__sendIcon'/>
+        <Send fontSize="small" className="preview__sendIcon" />
       </div>
     </div>
   );
