d1e3d7bb0543fa96e14947541b2e3a469fd2f4c9
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-29 20:42:37 -0500

    styled the preview component

--- src/Preview.js
+++ src/Preview.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
 import "./Preview.css";
 import CloseIcon from "@material-ui/icons/Close";
+import { AttachFile, Create, Crop, MusicNote, Note, Send, TextFields, Timer } from "@material-ui/icons";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -23,7 +24,20 @@ function Preview() {
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFields />
+        <Create />
+        <Note />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <Timer />
+      </div>
       <img src={cameraImage} alt="" />
+      <div className="preview__footer">
+        <h2>Send Now</h2>
+        <Send fontSize="small" className='preview__sendIcon'/>
+      </div>
     </div>
   );
 }
