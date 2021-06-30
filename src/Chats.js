63fb3ce6b49d36bae523dd462f92a590700b84b1
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-29 23:48:00 -0500

    styled chat component and started wiring it up.

--- src/Chats.js
+++ src/Chats.js
@@ -1,10 +1,39 @@
-import React from "react";
+import { Avatar } from "@material-ui/core";
+import { ChatBubble, Search } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
 import "./Chats.css";
+import { db } from "./firebase";
 
 function Chats() {
-  return <div className="chats">
-      
-  </div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <Search />
+          <input placeholder="Friends" type="text" />
+        </div>
+        <ChatBubble className="chats__chatIcon" />
+      </div>
+      <div className="chats__posts"></div>
+    </div>
+  );
 }
 
 export default Chats;
+// 2:00:30
