d581ad30524325eff51d6c779cd51c9f43594b4b
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-30 19:50:54 -0500

    finished building login component. started added featured to app for flow.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
@@ -6,36 +6,53 @@ import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
 import { useDispatch, useSelector } from "react-redux";
-import { selectUser } from "./features/appSlice";
+import { login, logout, selectUser } from "./features/appSlice";
 import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
-      {!user ? (
-        <Login />
-      ): (
-        <div className="app__body">
-          <Switch>
-            <Route exact path="/chats/view">
-              <ChatView />
-            </Route>
-            <Route exact path="/chats">
-              <Chats />
-            </Route>
-            <Route exact path="/preview">
-              <Preview />
-            </Route>
-            <Route exact path="/">
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
-      )}
-      
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route exact path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route exact path="/chats">
+                <Chats />
+              </Route>
+              <Route exact path="/preview">
+                <Preview />
+              </Route>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
